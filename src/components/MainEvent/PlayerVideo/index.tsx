270a77a14fb3e93b0b171eb4656171d8cc5c7b6c
Gustavo Mendel <gustavo.mendel.br@gmail.com>
2022-06-23 18:36:25 -0300

    feat: use Data getted on PlayerVideo

--- src/components/MainEvent/PlayerVideo/index.tsx
+++ src/components/MainEvent/PlayerVideo/index.tsx
@@ -1,14 +1,17 @@
-export function PlayerVideo() {
+interface VideoProps {
+  videoId?: string;
+}
+
+export function PlayerVideo(props: VideoProps) {
   return (
     <div className="bg-black flex justify-center">
       <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
         <iframe
           className="h-full w-full"
-          src="https://www.youtube.com/embed/KJj70dBgRPo"
+          src={`https://www.youtube.com/embed/${props.videoId}`}
           title="YouTube video player"
           frameBorder="0"
           allow="fullscreen; accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen={true}
         ></iframe>
       </div>
     </div>
