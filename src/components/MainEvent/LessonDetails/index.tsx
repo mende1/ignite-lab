8ea813dde38e35e15bc449bc3560732115274fe7
Gustavo Mendel <gustavo.mendel.br@gmail.com>
2022-06-23 18:36:07 -0300

    feat: use Data getted on LessonDetails

--- src/components/MainEvent/LessonDetails/index.tsx
+++ src/components/MainEvent/LessonDetails/index.tsx
@@ -1,25 +1,31 @@
-export function LessonDetails() {
+interface LessonDetailsProps {
+  title: string;
+  description: string;
+  teacher: {
+    name: string;
+    bio: string;
+    avatarURL: string;
+  };
+}
+
+export function LessonDetails(props: LessonDetailsProps) {
   return (
     <div className="flex-1">
-      <h1 className="text-2xl font-bold">
-        Aula 01 - Criando o projeto e realizando o setup inicial
-      </h1>
-      <p className="mt-4 text-gray-200 leading-relaxed">
-        Nessa aula vamos dar início ao projeto criando a estrutura base da
-        aplicação utilizando ReactJS, Vite e TailwindCSS. Vamos também realizar
-        o setup do nosso projeto no GraphCMS criando as entidades da aplicação e
-        integrando a API GraphQL gerada pela plataforma no nosso front-end
-        utilizando Apollo Client.
-      </p>
+      <h1 className="text-2xl font-bold">{props.title}</h1>
+      <p className="mt-4 text-gray-200 leading-relaxed">{props.description}</p>
       <div className="flex items-center gap-4 mt-6">
         <img
           className="h-16 w-16 rounded-full border-2 border-blue-500"
-          src="https://github.com/mende1.png"
-          alt="Foto de perfil do Professor"
+          src={props.teacher.avatarURL}
+          alt={`Foto de perfil do Professor ${props.teacher.name}`}
         />
         <div className="leading-relaxed">
-          <strong className="font-bold text-2xl block">Gustavo Mendel</strong>
-          <span className="text-gray-200 text-sm block">Software Engineer</span>
+          <strong className="font-bold text-2xl block">
+            {props.teacher.name}
+          </strong>
+          <span className="text-gray-200 text-sm block">
+            {props.teacher.name}
+          </span>
         </div>
       </div>
     </div>
