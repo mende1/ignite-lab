9d9cbff54cba4092eb90166ea8c7d667a6a19cab
Gustavo Mendel <gustavo.mendel.br@gmail.com>
2022-06-23 18:35:38 -0300

    feat: make the query and get the data about lesson

--- src/components/MainEvent/index.tsx
+++ src/components/MainEvent/index.tsx
@@ -1,15 +1,65 @@
+import { gql, useQuery } from "@apollo/client";
 import { ComunityButtons } from "./ComunityButtons";
 import { ExclusiveContents } from "./ExclusiveContents";
 import { LessonDetails } from "./LessonDetails";
 import { PlayerVideo } from "./PlayerVideo";
 
-export function MainEvent() {
+const GET_LESSON_BY_SLUG_QUERY = gql`
+  query GetLessonBySlug($slug: String) {
+    lesson(where: { slug: $slug }) {
+      title
+      description
+      videoId
+      teacher {
+        name
+        bio
+        avatarURL
+      }
+    }
+  }
+`;
+
+interface GetLessonBySlugQueryProps {
+  lesson: {
+    title: string;
+    description: string;
+    videoId: string;
+    teacher: {
+      name: string;
+      bio: string;
+      avatarURL: string;
+    };
+  };
+}
+
+interface MainEventProps {
+  lessonSlug: string;
+}
+
+export function MainEvent(props: MainEventProps) {
+  const { data } = useQuery<GetLessonBySlugQueryProps>(
+    GET_LESSON_BY_SLUG_QUERY,
+    {
+      variables: {
+        slug: props.lessonSlug,
+      },
+    }
+  );
+
+  if (!data) {
+    return <div className="flex-1">Carregando...</div>;
+  }
+
   return (
     <div className="flex-1">
-      <PlayerVideo />
+      <PlayerVideo videoId={data?.lesson.videoId} />
       <div className="p-8 max-w-[1100px] mx-auto">
         <div className="flex items-start gap-16">
-          <LessonDetails />
+          <LessonDetails
+            title={data?.lesson.title}
+            description={data?.lesson.description}
+            teacher={data?.lesson.teacher}
+          />
           <ComunityButtons />
         </div>
         <ExclusiveContents />
