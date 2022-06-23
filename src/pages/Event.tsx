9f6b850e691ab70ecc44e6ce8935810cb7272db3
Gustavo Mendel <gustavo.mendel.br@gmail.com>
2022-06-23 18:29:50 -0300

    feat: get slug Param and pass through application

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,13 +1,20 @@
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
 import { MainEvent } from "../components/MainEvent";
+import { useParams } from "react-router-dom";
 
 export function Event() {
+  const { slug } = useParams<{ slug: string }>();
+
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex flex-1">
-        <MainEvent />
+        {slug ? (
+          <MainEvent lessonSlug={slug} />
+        ) : (
+          <div className="flex-1"></div>
+        )}
         <Sidebar />
       </main>
     </div>
