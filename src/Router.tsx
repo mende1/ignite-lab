1456522f0768aff11f90b6849fc146e5226d1e85
Gustavo Mendel <gustavo.mendel.br@gmail.com>
2022-06-24 21:32:15 -0300

    feat: add Home page

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import { Event } from "./pages/Event";
+import { Home } from "./pages/Home";
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<a href="/event">Go to event</a>} />
+      <Route path="/" element={<Home />} />
       <Route path="/event" element={<Event />} />
       <Route path="/event/lesson/:slug" element={<Event />} />
     </Routes>
